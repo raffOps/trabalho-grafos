d9f19a31b81bc667236f0416ec0e36eaad6fc6bf
Rafael <rjribeiro@inf.ufrgs.br>
2017-12-08 17:02:06 -0200

    Finalizado o menu e a impressão das saidas

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
-
+infinito = 9999
 #vertex should start with zero
 #if you are giving weight above 999 adjust min in program
 #result will be the shortest path and the distace to each vertex from source vertex in order
-def dijkstra(matrix,m,n):
-    k=int(input("Enter the source vertex"))
-    cost=[[0 for x in range(m)] for x in range(1)]
-    offsets = []
-    offsets.append(k)
-    elepos=0
-    for j in range(m):
-        cost[0][j]=matrix[k][j]
-    mini=999
-    for x in range (m-1):
-        mini=999
-        for j in range(m):
-            print("{0} \t {1} \t {2}".format(cost, j, offsets))
-            if cost[0][j]<=mini and j not in offsets:
-                mini=cost[0][j]
-                elepos=j
-                print(elepos)
-        offsets.append(elepos)
-        print("\n")
-        for j in range (m):
-            if cost[0][j] >cost[0][elepos]+matrix[elepos][j]:
-                cost[0][j]=cost[0][elepos]+matrix[elepos][j]
-    print("The shortest path",offsets)
-    print("The cost to various vertices in order",cost)
+def dijkstra(matrix, quant_nodos, x, y, opcao):
+    tabela=[0 for nodo in range(quant_nodos)] # Criação da tabela de prioridade
+    nuvem = []
+    nuvem.append(x) # Adiciona o nodo inicial a nuvem
+    for j in range(quant_nodos): # Inicializa a tabela de prioridades com as distancias de x até seus vizinhos diretos
+        tabela[j] = matrix[x][j]
+
+    if opcao == 2:
+        print(
+            "Nodo sendo visitado: {0} \t Distancia para {1}: {2} \nTabela: {3} \nNuvem: {4}\n ".format(
+                nuvem[-1], x, tabela[nuvem[-1]], imprime_tabela(tabela, nuvem), nuvem))
+
+    for etapa in range(quant_nodos-1): # A quantidade total de etapas para completar a nuvem será a
+                                        # quantidades de nodos - 1
+        mini = infinito # Distancia mímima recebe um valor infinito
+        for j in range(quant_nodos): # Identificar o nodos mais perto do nodo atual
+            if tabela[j] <= mini and j not in nuvem: # Se o valor correspondente a j na tabela for menor que a
+                                                        # distancia minima atual e j não estiver na nuvem,
+                                                        # então mini é atualizado com o valor de j na tabela e
+                mini = tabela[j]
+                elepos = j
+        nuvem.append(elepos) # O nodo que estiver mais perto do nodo visitado anteriormente é adicionado na nuvem
+                                        # junto com a distancia entre eles
+        if elepos == y and opcao == 1:
+            print("Distância de {0} até {1}: {2}".format(x, y, mini))
+            return
+        for j in range(quant_nodos):
+            if tabela[j] > tabela[elepos] + matrix[elepos][j]:
+                tabela[j] = tabela[elepos] + matrix[elepos][j]
+        if opcao == 2:
+            print(
+                "Nodo sendo visitado: {0} \t Distancia para {1}: {2} \nTabela: {3} \nNuvem: {4}\n ".format(
+                    nuvem[-1], x, tabela[nuvem[-1]], imprime_tabela(tabela,nuvem),  nuvem))
+
+def imprime_tabela(tabela, nuvem):
+    tabela_copia = tabela.copy()
+    for x in range(len(tabela_copia)):
+        if x in nuvem:
+            tabela_copia[x] = '-'
+    return tabela_copia
+
 
 def main():
     with open("inst1.dat") as arquivo:  # abertura do arquivo
@@ -42,22 +59,21 @@ def main():
     for linha in range(quant_nodos):
         for coluna in range(quant_nodos):
             if matriz[linha][coluna] == 0:
-                matriz[linha][coluna] = 9999
+                matriz[linha][coluna] = infinito
 
-    print(matriz)
-
+    #print(matriz)
     continuar = True
     while continuar:
-        #x = int(input("\nDigite o nodo inicial: "))
-        #y = int(input("\nDigite o nodo final: "))
-        #print(quant_nodos)
-        dijkstra(matriz, quant_nodos, quant_nodos)
+        x = int(input("\nDigite o nodo inicial: "))
+        y = int(input("Digite o nodo final: "))
+        matriz[x][x] = 0
         opcao = 0
-        #while opcao != 1 or opcao != 2:
-            #print("\n\nOpção 1: Execução simples. Retorna a menor distancia entre o nodo x e nodo y")
-            #print("Opção 2: Execução completa. Mstra o estado do programa a cada iteração ")
-
-            #opcao = int(input("\nDigite sua opção: "))
+        while opcao != 1 and opcao != 2:
+            print("\nOpção 1: Execução simples. Retorna a menor distancia entre o nodo x e nodo y")
+            print("Opção 2: Execução completa. Mstra o estado do programa a cada iteração ")
+            opcao = int(input("\nDigite sua opção: "))
+        dijkstra(matriz, quant_nodos, x, y, opcao)
+        continuar = int(input("\nExecutar o dijkstra novamente? 1 para sim, 0 para Não: "))
 
 
 main()
