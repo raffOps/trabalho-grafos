df494a6519014686963f41ad4407da53b8d297a6
Caio Roberto <caiorrs@gmail.com>
2017-12-09 18:29:49 -0200

    alteração de menus

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def dijkstra(matrix, quant_nodos, x, y, opcao):
     for j in range(quant_nodos): # Inicializa a tabela de prioridades com as distancias de x ate seus vizinhos diretos
         tabela[j] = matrix[x][j]
 
-    if opcao == '2':
+    if opcao == 2:
         imprime_dados('1', tabela, nuvem, x)
 
     for etapa in range(quant_nodos-1): # A quantidade total de etapas para completar a nuvem sera a
@@ -36,7 +36,7 @@ def dijkstra(matrix, quant_nodos, x, y, opcao):
                 elepos = j
         nuvem.append(elepos) # O nodo que estiver mais perto do nodo visitado anteriormente eh adicionado na nuvem
                                         # junto com a distancia entre eles
-        if elepos == y and opcao == '1': # Se a opcao de saida for 1 e nodo adicionado na nuvem for y, entao a distancia
+        if elepos == y and opcao == 1: # Se a opcao de saida for 1 e nodo adicionado na nuvem for y, entao a distancia
                                        #  é printada e programa eh finalizado
             print('Distancia de {0} até {1}: {2}'.format(x, y, mini))
             return
@@ -76,29 +76,39 @@ def imprime_dados(etapa, tabela, nuvem, x):
 
 
 def main():
-    try:
-        arquivo = open(input('Digite o nome do arquivo: '))  # abertura do arquivo
-    except FileNotFoundError:
-        print('Arquivo nao encontrado')
+    opcao_menu1 = -1
+    while opcao_menu1 < 0 or opcao_menu1 > 1:
+        print('\nOpção 0: Encerrar programa')
+        print('Opção 1: Ler arquivo')
+        opcao_menu1 = int(input('Digite sua opção: '))
+    if opcao_menu1 == 0:
+        return
     else:
-        with arquivo:
-            primeira_linha = next(arquivo).split(' ')
-            quant_nodos, quant_arestas = int(primeira_linha[0]), int(primeira_linha[1])  # extracao da primeira linha
+        try:
+            arquivo = open(input('Digite o nome do arquivo: '))  # abertura do arquivo
+        except FileNotFoundError:
+            print('Arquivo nao encontrado')
+        else:
+            with arquivo:
+                primeira_linha = next(arquivo).split(' ')
+                quant_nodos, quant_arestas = int(primeira_linha[0]), int(
+                    primeira_linha[1])  # extracao da primeira linha
 
-            matriz = [[0 for x in range(quant_nodos)] for x in range(quant_nodos)]  # criacao da matriz de acordo com os
-            # dados da primeira linha
-            for linha in arquivo:  # Valores sendo atribuidos a matriz
-                linha = linha.split(' ')
-                matriz[int(linha[0])][int(linha[1])] = int(linha[2])
-                matriz[int(linha[1])][int(linha[0])] = int(linha[2])
+                matriz = [[0 for x in range(quant_nodos)] for x in
+                          range(quant_nodos)]  # criacao da matriz de acordo com os
+                # dados da primeira linha
+                for linha in arquivo:  # Valores sendo atribuidos a matriz
+                    linha = linha.split(' ')
+                    matriz[int(linha[0])][int(linha[1])] = int(linha[2])
+                    matriz[int(linha[1])][int(linha[0])] = int(linha[2])
 
-        for linha in range(quant_nodos): # Troca valores 0 por valor que representa o infinito
-            for coluna in range(quant_nodos):
-                if matriz[linha][coluna] == 0:
-                    matriz[linha][coluna] = infinito
+            for linha in range(quant_nodos):  # Troca valores 0 por valor que representa o infinito
+                for coluna in range(quant_nodos):
+                    if matriz[linha][coluna] == 0:
+                        matriz[linha][coluna] = infinito
 
-        #for linha in matriz:
-        #   print(linha)
+            # for linha in matriz:
+            #   print(linha)
 
         continuar = True
         while continuar:
